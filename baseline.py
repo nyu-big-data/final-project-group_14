269ca5e7fe53c120330368e5ffbed5f9c0de9a8f
shambhavi rege <shambhavirege@shambhavis-mbp.myfiosgateway.com>
2022-04-29 00:10:45 -0400

    finalcheckpoint

--- baseline.py
+++ baseline.py
@@ -1,17 +1,14 @@
-import getpass
-import math
-import pyspark
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
-from pyspark.sql import Row
-from pyspark import SparkConf, SparkContext
-# And pyspark.sql to get the spark session
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark.mllib.evaluation import RankingMetrics
 import sys 
-import numpy
 
 def main(spark, file_path):
+    
+    
+    #Reading train, val and test CSVs or Parquet files.
     
     #train_ratings = spark.read.csv(file_path+'/ratings_train_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
     #val_ratings = spark.read.csv(file_path+'/ratings_valid_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
@@ -21,39 +18,37 @@ def main(spark, file_path):
     val_ratings = spark.read.parquet(file_path+'/ratings_valid_splits.parquet')
     test_ratings = spark.read.parquet(file_path+'/ratings_test_splits.parquet')
     
-    
     train_ratings.createOrReplaceTempView('train_ratings')
     val_ratings.createOrReplaceTempView('val_ratings')
     test_ratings.createOrReplaceTempView('test_ratings')
     
-    
-    
     train_ratings = spark.sql("SELECT movieId, rating FROM train_ratings")
     
+    #Partitioning the data to find average ratings for each movie in train ratings
     w = Window.partitionBy('movieId')
     top_100 = train_ratings.withColumn("avg_rating", F.avg("rating").over(w)).orderBy("avg_rating", ascending=False).limit(100)
     top_100.show()
     
     
-    
+    #Storing top 100 movieIds as a list
     val_ratings = val_ratings.groupBy("userId").agg(F.collect_list("movieId").alias("movieIds"))
     top_100 = top_100.select('movieId').rdd.flatMap(lambda x: x).collect()
+    
     
     eval_list = []
     for row in val_ratings.rdd.collect():
         
         eval_list.append((top_100, row.movieIds))
 
-    
-    
     sc =  SparkContext.getOrCreate()
-        
+     
+    #Evaluation on val
     predictionAndLabels = sc.parallelize(eval_list)
     metrics = RankingMetrics(predictionAndLabels)
+    
     print(metrics.meanAveragePrecisionAt(100))
     print(metrics.meanAveragePrecision)
     print(metrics.precisionAt(100))
-    
     
     test_ratings = test_ratings.groupBy("userId").agg(F.collect_list("movieId").alias("movieIds"))
     
@@ -61,24 +56,14 @@ def main(spark, file_path):
     for row in test_ratings.rdd.collect():
         
         eval_list2.append((top_100, row.movieIds))
-
+        
+    #Evaluation on test    
     predictionAndLabels = sc.parallelize(eval_list2)
     metrics2 = RankingMetrics(predictionAndLabels)
     
     print(metrics2.meanAveragePrecisionAt(100))
     print(metrics2.meanAveragePrecision)
     print(metrics2.precisionAt(100))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 
 if __name__ == "__main__":
--- partitioning-2.py
+++ partitioning-2.py
@@ -8,13 +8,8 @@ Created on Sun Apr 24 15:25:28 2022
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#Use getpass to obtain user netID
-import getpass
-#import math
-#from pyspark.sql import functions as F
-#from pyspark.sql.window import Window
-#from pyspark.sql import Row
-# And pyspark.sql to get the spark session
+
+
 from pyspark.sql import SparkSession
 import sys
 
@@ -24,24 +19,25 @@ def partition(spark, file_path):
     Parameters
     ----------
     spark : SparkSession object
-    netID : string, netID of student to find files in HDFS
+    file_path: string path to read and write CSV and Parquet files
     '''
-
-
     
-
+    #Reading CSV and Parquet files
     movie_ratings = spark.read.csv(file_path+'/ratings.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp DOUBLE')
     #movie_ratings = spark.read.parquet(file_path+'/ratings.parquet')
     movie_ratings.show()
     
     movie_ratings.createOrReplaceTempView('movie_ratings')
     
+    #Splitting distinct userIds into proportions as follows
     partition = movie_ratings.select('userId').distinct().randomSplit([0.4, 0.3, 0.3], seed = 1234)
+    
     
     train_users = tuple(list(x.userId for x in partition[0].collect()))
     val_users = tuple(list(x.userId for x in partition[1].collect()))
     test_users = tuple(list(x.userId for x in partition[2].collect()))
     
+    #Making spark data frames for the above splits
     train = spark.sql("SELECT * FROM movie_ratings WHERE userId in "+ str(train_users))
     test = spark.sql("SELECT * FROM movie_ratings WHERE userId in "+ str(test_users))
     val = spark.sql("SELECT * FROM movie_ratings WHERE userId in "+ str(val_users))
@@ -50,22 +46,21 @@ def partition(spark, file_path):
     test.createOrReplaceTempView('test')
     val.createOrReplaceTempView('val')
     
-    
+    #Finding 60th percentile of our spark dataframes so as to further divide them into future trends and historic trends
     test_train = spark.sql("SELECT userId, PERCENTILE(timestamp, 0.6) as threshold FROM test GROUP BY userId ")
     val_train = spark.sql("SELECT userId, PERCENTILE(timestamp, 0.6) as threshold FROM val GROUP BY userId ")
     
     test_train.createOrReplaceTempView('test_train')
     val_train.createOrReplaceTempView('val_train')
     
-    
+    #Appending historic records to training datasets from validation data and test data each
     test_train = spark.sql("SELECT t.userId, t.movieId, t.rating, t.timestamp FROM test as t INNER JOIN test_train as tt ON t.userId = tt.userID WHERE t.timestamp <= tt.threshold")
     val_train = spark.sql("SELECT v.userId, v.movieId, v.rating, v.timestamp FROM val as v INNER JOIN val_train as vt ON v.userId = vt.userID WHERE v.timestamp <= vt.threshold")
-    
-    
     
     train = train.union(test_train)
     train = train.union(val_train)
     
+    #Keeping future trends as our held out data in test and val 
     test = test.subtract(test_train)
     val = val.subtract(val_train)
     
@@ -73,8 +68,7 @@ def partition(spark, file_path):
     test.orderBy('userId').groupBy('userId').count().show()
     val.orderBy('userId').groupBy('userId').count().show()
     
-    
-    
+    #Writting them to CSVs and Parquets
     train.write.csv(file_path+'/ratings_train_splits.csv', header=True)
     val.write.csv(file_path+'/ratings_valid_splits.csv', header=True)
     test.write.csv(file_path+'/ratings_test_splits.csv', header=True)
@@ -84,66 +78,6 @@ def partition(spark, file_path):
     #test.write.parquet(file_path+'/ratings_test_splits.parquet')
     
     
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    #movies = spark.read.csv('hdfs:/user/sr6172/movies.csv', header = True, schema = 'movieId INT, title STRING, genres STRING')
-    #movie_ratings = movie_ratings.join(movies, on='movieId', how='inner')
-    
-    #movie_ratings.groupBy("userId").count().show()
-    #train=movie_ratings.sampleBy("userId", fractions={ }, seed=1234)
-    #train.groupBy("userId").count().show()
-    #train.show()
-    
-    #test=movie_ratings.subtract(train)
-    #test.groupBy("userId").count().show()
-    
-    #test.orderBy('userId').show()
-    
-    #window = Window.partitionBy('userId').orderBy('')
-    #test = test.select('userId','movieId','rating','timestamp', F.row_number().over(window).alias("row_number"))
-    
-    #test_split = test.filter(test.userId % 2 == 1)
-    #val_split = test.filter(test.userId % 2 == 0)
-    
-    #test_split.show()
-    #val_split.show()
-    
-    
-    
-
-    
-
-
 # Only enter this block if we're in main
 if __name__ == "__main__":
 
