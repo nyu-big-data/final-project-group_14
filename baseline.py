d6d907650e121958419dfdcf3df800bdfebd4f82
shambhavi rege <shambhavirege@shambhavis-mbp.myfiosgateway.com>
2022-04-28 22:05:08 -0400

    part

--- baseline.py
+++ baseline.py
@@ -5,10 +5,38 @@ from pyspark.sql.window import Window
 from pyspark.sql import Row
 # And pyspark.sql to get the spark session
 from pyspark.sql import SparkSession
-def main(spark):
-    movie_ratings = spark.read.csv('hdfs:/user/sa6523/ratings.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+def main(spark, file_path):
+    
+    train_ratings = spark.read.csv(file_path+'/ratings_train_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    val_ratings = spark.read.csv(file_path+'/ratings_val_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    test_ratings = spark.read.csv(file_path+'/ratings_test_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    
+    train_ratings.createOrReplaceTempView('train_ratings')
+    val_ratings.createOrReplaceTempView('val_ratings')
+    test_ratings.createOrReplaceTempView('test_ratings')
+    
+    
+    
+    train_ratings = spark.sql("SELECT movieId, rating FROM train_ratings")
+    
     w = Window.partitionBy('movieId')
-    movie_ratings.withColumn("avg_rating", F.avg("rating").over(w)).orderBy("avg_rating", ascending=False).show()
+    top_100 = train_ratings.withColumn("avg_rating", F.avg("rating").over(w)).orderBy("avg_rating", ascending=False).limit(100)
+    top_100.show()
+    
+    
+    
+    val_ratings.groupBy("userId").agg(F.collect_list("movieId").alias("movieIds")).show()
+    
+    top_100 = top_100.select('movieId').rdd.flatMap(lambda x: x).collect()
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 if __name__ == "__main__":
     
