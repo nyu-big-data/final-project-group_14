8c13fff84b46dd96202b847dd00c4cce8d0620d9
shambhavi rege <shambhavirege@macbook-pro.myfiosgateway.com>
2022-05-13 12:53:42 -0400

    final

--- ALS.py
+++ ALS.py
@@ -22,10 +22,15 @@ def main(spark, file_path):
     
     #Reading train, val and test CSVs or Parquet files.
     
-    train_ratings = spark.read.csv(file_path+'/ratings_train_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
-    val_ratings = spark.read.csv(file_path+'/ratings_valid_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
-    test_ratings = spark.read.csv(file_path+'/ratings_test_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
-    ratings = spark.read.csv(file_path+'/ratings.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    #train_ratings = spark.read.csv(file_path+'/ratings_train_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    #val_ratings = spark.read.csv(file_path+'/ratings_valid_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    #test_ratings = spark.read.csv(file_path+'/ratings_test_splits.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    #ratings = spark.read.csv(file_path+'/ratings.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp INT')
+    
+    train_ratings = spark.read.parquet(file_path+'/ratings_train_splits.parquet')
+    val_ratings = spark.read.parquet(file_path+'/ratings_valid_splits.parquet')
+    test_ratings = spark.read.parquet(file_path+'/ratings_test_splits.parquet')
+    ratings = spark.read.parquet(file_path+'/ratings.parquet')
     
     train_ratings.createOrReplaceTempView('train_ratings')
     val_ratings.createOrReplaceTempView('val_ratings')
