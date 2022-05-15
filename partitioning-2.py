02e1d2fd156ccbd67075d792eb19e90622a98a15
Shriya Murthy <shriyamurthy@shriyas-mbp.myfiosgateway.com>
2022-05-15 14:41:48 -0400

    trial

--- partitioning-2.py
+++ partitioning-2.py
@@ -23,8 +23,8 @@ def partition(spark, file_path):
     '''
     
     #Reading CSV and Parquet files
-    movie_ratings = spark.read.csv(file_path+'/ratings.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp DOUBLE')
-    #movie_ratings = spark.read.parquet(file_path+'/ratings.parquet')
+    #movie_ratings = spark.read.csv(file_path+'/ratings.csv', header = True ,schema = 'userId INT, movieId INT, rating FLOAT, timestamp DOUBLE')
+    movie_ratings = spark.read.parquet(file_path+'/ratings.parquet')
     movie_ratings.show()
     
     movie_ratings.createOrReplaceTempView('movie_ratings')
@@ -69,13 +69,13 @@ def partition(spark, file_path):
     val.orderBy('userId').groupBy('userId').count().show()
     
     #Writting them to CSVs and Parquets
-    train.write.csv(file_path+'/ratings_train_splits.csv', header=True)
-    val.write.csv(file_path+'/ratings_valid_splits.csv', header=True)
-    test.write.csv(file_path+'/ratings_test_splits.csv', header=True)
+    #train.write.csv(file_path+'/ratings_train_splits.csv', header=True)
+    #val.write.csv(file_path+'/ratings_valid_splits.csv', header=True)
+    #test.write.csv(file_path+'/ratings_test_splits.csv', header=True)
     
-    #train.write.parquet(file_path+'/ratings_train_splits.parquet')
-    #val.write.parquet(file_path+'/ratings_valid_splits.parquet')
-    #test.write.parquet(file_path+'/ratings_test_splits.parquet')
+    train.write.parquet(file_path+'/ratings_train_splits.parquet')
+    val.write.parquet(file_path+'/ratings_valid_splits.parquet')
+    test.write.parquet(file_path+'/ratings_test_splits.parquet')
     
     
 # Only enter this block if we're in main
